17bbcb95f163ee4897a763125ae4775a05962766
Abhinav <abhinavchowdary79@gmail.com>
2025-07-28 16:38:58 +0530

    test

--- flask-api/app.py
+++ flask-api/app.py
@@ -1,14 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Welcome to Flask App in Docker!'
-
-@app.route('/health')
-def health():
-    return 'OK'
+def hello():
+    return "Hello from Dockerized Flask!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
